512432ef2568da5476c45b6d18f369c94754ab30
Alexei <asiventsev@gmail.com>
2019-04-22 18:47:46 +0500

    issue create/update (subject, start_date, due_date)

--- src/p2r.rb
+++ src/p2r.rb
@@ -58,18 +58,6 @@ missed_pars = %w(redmine_host redmine_api_key redmine_project_uuid task_redmine_
 
 chk !missed_pars.empty?, "ERROR: following settings not found in 'Redmine Sysncronization' task: #{missed_pars.sort.join ', '}"
 
-def rm_request pak, path, data=nil
-  host, port = pak['redmine_host'].split ':'
-  reply = Net::HTTP.start(pak['redmine_host'], pak['redmine_port']) do |http|
-    if data
-      http.request_post(path,data.to_json, 'Content-Type'=>'application/json', 'X-Redmine-API-Key' => pak['redmine_api_key'])
-    else
-      http.request_get(path, 'X-Redmine-API-Key' => pak['redmine_api_key'])
-    end
-  end
-  return reply
-end
-
 #---------------------------------------------------------------------
 # check Redmine project availability
 #---------------------------------------------------------------------
@@ -115,35 +103,76 @@ case re.code
     chk true, "ERROR: #{re.code} #{re.message}"
 end
 
-if rmp_id
-  #=====================================================================
-  # existing Redmine project update
-  #---------------------------------------------------------------------
+def build_mst_url(pak, rmt_id); pak['task_redmine_url_field'] ? "http://#{pak['redmine_host']}:#{pak['redmine_port']}/issues/#{rmt_id}" : nil; end
+def get_mst_url(pak, mst); pak['task_redmine_url_field'] ? eval("mst.#{pak['task_redmine_url_field']}") : nil; end
+def set_mst_url(pak, mst, rmt_id)
+  url=build_mst_url pak, rmt_id
+  eval("mst.#{pak['task_redmine_url_field']} = '#{url}'") if url
+  return url
+end
+def get_mst_redmine_id(pak, mst); eval("mst.#{pak['task_redmine_id_field']}"); end
+def set_mst_redmine_id(pak, mst, rmt_id); eval("mst.#{pak['task_redmine_id_field']} = '#{rmt_id}'"); end
 
-  puts 'existing Redmine project update'
+def process_issues pak, msp, rmp_id, force_task_creation = false
   rmt_id_field = "mst.#{pak['task_redmine_id_field']}"
   (1..msp.Tasks.Count).each do |i|
     next unless mst = msp.Tasks(i)
     rmt_id = eval(rmt_id_field)
     next if rmt_id.empty? # task not marked for sync
     rmt_id = rmt_id.to_i rescue 0
-    if rmt_id == 0
+    if rmt_id == 0 || force_task_creation
       # create new task
       unless DRY_RUN
-        rmt={
-            project_id: rmp_id, subject: mst.Name, description: "-----\nSynchronized with task #{mst.ID} from MSP project #{msp.Name}\n-----\n",
+        rmt = {
+            project_id: rmp_id, subject: mst.Name, description: "-----\nAutocreated by P2R from MSP task #{mst.ID} in MSP project #{msp.Name}\n-----\n",
             start_date: mst.Start.strftime('%Y-%m-%d'), due_date: mst.Finish.strftime('%Y-%m-%d')
         }
-        puts rmt
-        re = rm_request pak, '/issues.json', issue: rmt
-        puts '- - - -',re.code,re.msg,re.body.inspect
+        rmt = rm_create pak, '/issues.json', 'issue', rmt,
+                        "ERROR: could not create Redmine task from #{mst.ID} #{mst.Name} for some reasons"
+        # write new task number to MSP
+        set_mst_redmine_id pak, mst, rmt['id']
+        set_mst_url pak, mst, rmt['id']
       else
-        # keep task to be updated
+        # keep task to be created
+        puts "Will create task #{mst.ID} #{mst.Name}"
       end
     else
       # update existing task
+      #   check task availability
+      rmt = rm_get pak, "/issues/#{rmt_id}.json", 'issue', "ERROR: could not find Redmine task ##{rmt_id} for #{mst.ID} #{mst.Name}"
+      #   check for changes
+      #     subject - Name, start_date - Start, due_date - Finish
+      changes={}
+      changes['subject'] = mst.Name if rmt['subject'] != mst.Name.encode('UTF-8')
+      d = mst.Start.strftime('%Y-%m-%d')
+      changes['start_date'] = d if rmt['start_date'] != d
+      d = mst.Finish.strftime('%Y-%m-%d')
+      changes['due_date'] = d if rmt['due_date'] != d
+      if changes.empty?
+        puts "No changes for Task Redmine ##{rmt_id} from MSP #{mst.ID} #{mst.Name}"
+      else
+        changelist = changes.keys.join(', ')
+        changes['notes'] = "Autoupdated by P2P at #{Time.now.strftime '%Y-%m-%d %H:%M'} (#{changelist})"
+        if DRY_RUN
+          puts "Will update task Redmine ##{rmt_id} from MSP #{mst.ID} #{mst.Name} (#{changelist})"
+        else
+          rm_update pak, "/issues/#{rmt['id']}.json", 'issue', {issue: changes},
+                    "ERROR: could not update Redmine task ##{rmt['id']} from #{mst.ID} #{mst.Name} for some reasons"
+          puts "Updated task Redmine ##{rmt_id} from MSP #{mst.ID} #{mst.Name} (#{changelist})"
+        end
+      end
+      set_mst_url pak, mst, rmt['id']
     end
   end
+end
+
+if rmp_id
+  #=====================================================================
+  # existing Redmine project update
+  #---------------------------------------------------------------------
+
+  puts 'Existing Redmine project update'
+  process_issues pak, msp, rmp_id, false
 
 else
   #=====================================================================
@@ -151,30 +180,28 @@ else
   #---------------------------------------------------------------------
   if DRY_RUN
     # project creation requested - exit on dry run
-    chk true, 'DRY RUN SUCCESS: new project to be created in Redmine'
+    chk true, "Will create new Redmine project #{pak['redmine_project_uuid']} from MSP project #{msp.Title}"
   end
 
   #---------------------------------------------------------------------
   # new Redmine project create
   #---------------------------------------------------------------------
   rmp = {name: msp.Title, identifier: pak['redmine_project_uuid'], is_public: false}
-  re = rm_request pak, '/projects.json',project: rmp
-  msg='ERROR: could not create Redmine project for some reasons'
-  chk (re.code!='201'), msg
-  rmp = JSON.parse(re.body)['project'] rescue nil
-  chk !rmp, msg
+  rmp = rm_create pak, '/projects.json', 'project', rmp,
+      'ERROR: could not create Redmine project for some reasons'
 
   # add rm project id to msp settings
   pak['redmine_project_id'] = rmp['id']
   settings_task.Notes = YAML.dump pak
-  puts 'new Redmine project created'
+  puts "Created new Redmine project #{pak['redmine_project_uuid']} ##{rmp['id']} from MSP project #{msp.Title}"
 
   #---------------------------------------------------------------------
   # add tasks to Redmine project
   #---------------------------------------------------------------------
-
+  process_issues pak, msp, rmp['id'], true
 
 end
 
+puts "\n\n"
 
 
--- src/p2r_lib.rb
+++ src/p2r_lib.rb
@@ -27,3 +27,65 @@ HELP=
 #-----------------------------------------------------------------------------------------------------
 def chk cond,msg;if cond;puts msg+"\n\n";exit 0;end;end
 
+#-----------------------------------------------------------------------------------------------------
+# REST to Redmine API
+#-----------------------------------------------------------------------------------------------------
+def rm_request pak, path, data = nil,method = nil, verbose = false
+  reply = nil
+  hdrs = {'Content-Type'=>'application/json', 'X-Redmine-API-Key' => pak['redmine_api_key']}
+  reply = Net::HTTP.start(pak['redmine_host'], pak['redmine_port']) do |http|
+    if data
+      if !method || method == 'POST'
+        puts "POST #{path}" if verbose
+        http.request_post(path,data.to_json, hdrs)
+      elsif method == 'PUT'
+        # code for PUT here
+        puts "PUT #{path}" if verbose
+        http.send_request 'PUT', path, JSON.unparse(data), hdrs
+      end
+    else
+      puts "GET #{path}" if verbose
+      http.request_get(path, hdrs)
+    end
+  end
+  return reply
+end
+
+#-----------------------------------------------------------------------------------------------------
+# REST get from Redmine API
+#-----------------------------------------------------------------------------------------------------
+def rm_get pak, path, entity, msg
+
+  re = rm_request pak, path
+  chk (re.code!='200'), msg + "\n#{re.code} #{re.msg}\n\n"
+  rmp = JSON.parse(re.body)[entity] rescue nil
+  chk !rmp, msg + "\n#{re.body.inspect}\n\n"
+  return rmp
+
+end
+
+#-----------------------------------------------------------------------------------------------------
+# REST create to Redmine API
+#-----------------------------------------------------------------------------------------------------
+def rm_create pak, path, entity, data, msg
+
+  re = rm_request pak, path, entity => data
+  chk (re.code!='201'), msg + "\n#{re.code} #{re.msg}\n\n"
+  rmp = JSON.parse(re.body)[entity] rescue nil
+  chk !rmp, msg + "\n#{re.body.inspect}\n\n"
+  return rmp
+
+end
+
+#-----------------------------------------------------------------------------------------------------
+# REST update to Redmine API
+#-----------------------------------------------------------------------------------------------------
+def rm_update pak, path, entity, data, msg
+
+  re = rm_request pak, path, data, 'PUT'
+  chk (re.code!='200'), msg + "\n#{re.code} #{re.msg}\n\n"
+  return true
+
+end
+
+
